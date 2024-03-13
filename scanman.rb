fcbc2f5812df9386961c7b1b076e779ab2e5c809
Nikhil Kumar <nikhilkmr300@gmail.com>
2024-03-12 21:42:42 -0700

    Fixed setup.py

--- scanman.rb
+++ scanman.rb
@@ -2,8 +2,8 @@ class Scanman < Formula
   include Language::Python::Virtualenv
 
   desc "Using LLMs to interact with man pages"
-  url "https://github.com/nikhilkmr300/scanman/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "0682e815dc203514a42a8eb0d8808ba00b4ed636849afe2f2295ea4154f7f0e8"
+  url "https://github.com/nikhilkmr300/scanman/archive/refs/tags/1.0.3.tar.gz"
+  sha256 "9e90b570b5e494558dd695b5a9db389a9bb7666f76fba39f04f2b85c3a3412fc"
 
   depends_on "python@3.11"
 
--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name="scanman",
     description="Using LLMs to interact with man pages",
     url="https://github.com/nikhilkmr300/scanman",
+    version="1.0.3",
     author="Nikhil Kumar",
     author_email="nikhilkmr300@gmail.com",
     license="MIT",
-    packages=find_packages(where="src"),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     install_requires=[
         "faiss-cpu",
         "langchain",
